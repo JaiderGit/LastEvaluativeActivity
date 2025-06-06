ee562c47d4505fe84e98a517406193f1a86b63cd
JAIDER ALBERTO TABORDA M�NERA <jaideratm@gmail.com>
2025-06-05 22:08:11 -0500

    Create Logic BillService.cs

--- Facturacion/Facturacion/Domain/Services/BillService.cs
+++ Facturacion/Facturacion/Domain/Services/BillService.cs
@@ -1,6 +1,74 @@
-﻿namespace Facturacion.Domain.Services
+﻿using Facturacion.DAL;
+using Facturacion.DAL.Entities;
+using Facturacion.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Facturacion.Domain.Services
 {
-    public class BillService
+    public class BillService : IBillService
     {
+        private readonly DataBaseContext _context;
+
+        public BillService(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Bill>> GetBillAsync()
+        {
+            var bills= await _context.Bills.ToListAsync();
+            return bills;
+        }
+
+        public async Task<Bill> GetBillByIdAsync(Guid id)
+        {
+            var bill = await _context.Bills.FirstOrDefaultAsync(c => c.Id == id);
+            return bill;
+        }
+
+        public async Task<Bill> CreateBillAsync(Bill bill)
+        {
+            try
+            {
+                bill.Id = Guid.NewGuid();
+                //value
+                //document
+                bill.CreatedDate = DateTime.Now;
+                _context.Bills.Add(bill);
+                //Client
+
+                await _context.SaveChangesAsync();
+                return bill;
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                throw new Exception(dbUpdateException.InnerException?.Message ?? dbUpdateException.Message);
+            }
+        }
+
+        public async Task<Bill> EditBillAsync(Bill bill)
+        {
+            bill.ModifiedDate = DateTime.Now;
+            _context.Bills.Update(bill);
+            await _context.SaveChangesAsync();
+            return bill;
+        }
+
+        public async Task<Bill> DeleteBillAsync(Guid id)
+        {
+            var bill = await GetBillByIdAsync(id);
+
+            if (bill == null) return null;
+
+             _context.Bills.Remove(bill);
+            await _context.SaveChangesAsync();
+            
+            return bill;
+        }
+
+        
+
+        
+        
     }
 }
