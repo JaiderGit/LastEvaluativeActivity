7e3763a73f3771dce1621536b236425b1fbb3658
JAIDER ALBERTO TABORDA M�NERA <jaideratm@gmail.com>
2025-06-05 21:25:47 -0500

    IBillService.cs Created

---
+++ Facturacion/Facturacion/Domain/Interfaces/IBillService.cs
@@ -0,0 +1,18 @@
+﻿using Facturacion.DAL.Entities;
+
+namespace Facturacion.Domain.Interfaces
+{
+    public interface IBillService
+    {
+        Task<IEnumerable<Bill>> GetBillAsync(); //Todas las facturas
+
+        Task<Bill> CreateBillAsync(Bill bill); //
+
+        Task<Bill> GetBillByIdAsync(Guid id);
+
+        Task<Bill> EditBillAsync(Bill bill);
+
+        Task<Bill> DeleteBillAsync(Guid id);
+
+    }
+}
