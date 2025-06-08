017e2f2d521ee9699b518a8c75e8fff122eb83a6
JAIDER ALBERTO TABORDA M�NERA <jaideratm@gmail.com>
2025-06-08 06:44:49 -0500

    Finish API Bills

---
+++ Facturacion/Facturacion/DAL/SeederDB.cs
@@ -0,0 +1,52 @@
+﻿using Facturacion.DAL.Entities;
+
+namespace Facturacion.DAL
+{
+    public class SeederDB
+    {
+        private readonly DataBaseContext _context;
+        public SeederDB(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeederAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await BillsNewPeriotAsync();
+
+            await _context.SaveChangesAsync();
+        }
+
+        private async Task BillsNewPeriotAsync()
+        {
+            if (!_context.Bills.Any())
+            {
+                _context.Bills.Add(new Entities.Bill
+                {
+                    CreatedDate = DateTime.Now,
+                    Document = "MED01",
+                    Value = 100000,
+                    Client = "Jaider",
+                    creditNotes = new List<CreditNote>()
+                    {
+                        new CreditNote
+                        {
+                            CreatedDate=DateTime.Now,
+                            Motive="Error de Precio",
+                            Value=10000
+                        }
+                    }
+                });
+                _context.Bills.Add(new Entities.Bill
+                {
+                    CreatedDate = DateTime.Now,
+                    Document = "MED02",
+                    Value = 300000,
+                    Client = "Alexander"
+                });
+            }
+        }
+
+    }
+}
--- Facturacion/Facturacion/Program.cs
+++ Facturacion/Facturacion/Program.cs
@@ -13,13 +13,31 @@ builder.Services.AddDbContext<DataBaseContext>(o => o.UseSqlServer(builder.Confi
 
 builder.Services.AddScoped < IBillService,BillService>();
 builder.Services.AddScoped<ICreditNoteService, CreditNoteService>();
+builder.Services.AddTransient<SeederDB>();
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
 var app = builder.Build();
+
+SeederData();
+
+void SeederData()
+{
+    IServiceScopeFactory? scopeFactory= app.Services.GetService<IServiceScopeFactory>();
+
+    using (IServiceScope? scope = scopeFactory.CreateScope())
+    {
+        SeederDB? service=scope.ServiceProvider.GetService<SeederDB>();
+        service.SeederAsync().Wait();
+    }
+
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
